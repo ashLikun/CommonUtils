3d62f1dc214ae99c17905eed5042e904a4b74595
likun <496546144@qq.com>
2017-10-09 23:54:02 +0800

    优化

--- utilslib/src/main/java/com/ashlikun/utils/other/DeviceUtil.java
+++ utilslib/src/main/java/com/ashlikun/utils/other/DeviceUtil.java
@@ -28,6 +28,31 @@ import java.util.UUID;
  * <p>
  * 功能介绍：获取设备信息的工具类
  * 权限：android.permission.TELEPHONY_SERVICE   ，  READ_PHONE_STATE
+ *
+ *   public String getTextAAA() {
+ StringBuffer sb = new StringBuffer();
+ sb.append("主板： "+ Build.BOARD);
+ sb.append("\n系统启动程序版本号： "+ Build.BOOTLOADER);
+ sb.append("\n系统定制商： "+ Build.BRAND);
+ sb.append("\ncpu指令集： "+ Build.CPU_ABI);
+ sb.append("\ncpu指令集2 "+ Build.CPU_ABI2);
+ sb.append("\n设置参数： "+ Build.DEVICE);
+ sb.append("\n显示屏参数：" + Build.DISPLAY);
+ sb.append("\n无线电固件版本：" + Build.getRadioVersion());
+ sb.append("\n硬件识别码： "+ Build.FINGERPRINT);
+ sb.append("\n硬件名称： "+ Build.HARDWARE);
+ sb.append("\nHOST: "+ Build.HOST);
+ sb.append("\nBuild.ID);"+ Build.ID);
+ sb.append("\n硬件制造商： "+ Build.MANUFACTURER);
+ sb.append("\n版本： "+ Build.MODEL);
+ sb.append("\n硬件序列号： "+ Build.SERIAL);
+ sb.append("\n手机制造商： "+ Build.PRODUCT);
+ sb.append("\n  描述Build的标签： "+ Build.TAGS);
+ sb.append("\nTIME: "+ Build.TIME);
+ sb.append("\nbuilder类型"+Build.TYPE);
+ sb.append("\nUSER: "+Build.USER);
+ return sb.toString();
+ }
  */
 
 public class DeviceUtil {
