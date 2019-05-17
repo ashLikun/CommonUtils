2a369a6cebee9424f2626864cfcabee016d7f9dd
likun <496546144@qq.com>
2019-05-17 15:26:51 +0800

    优化SuperToast

--- utilslib/src/main/java/com/ashlikun/utils/ui/SuperToast.java
+++ utilslib/src/main/java/com/ashlikun/utils/ui/SuperToast.java
@@ -41,7 +41,6 @@ import static com.ashlikun.utils.AppUtils.getApp;
 
 public class SuperToast {
     private static Toast mToast = null;
-    private static final String TOAST_VIEW_TAG = "TOAST_VIEW_TAG";
     private static final int Info = 1;//正常
     private static final int Confirm = 2;//完成
     private static final int Warning = 3;//警告 orange
@@ -93,11 +92,7 @@ public class SuperToast {
     private void cretae(final Builder builder) {
         initToast();
         View mView;
-        if (mToast.getView() != null && TOAST_VIEW_TAG.equals(mToast.getView().getTag())) {
-            mView = mToast.getView();
-        } else {
-            mView = LayoutInflater.from(getApp()).inflate(builder.layoutId, null);
-        }
+        mView = LayoutInflater.from(getApp()).inflate(builder.layoutId, null);
         setViewContent(mView, builder);
         startAnim(builder, mView);
         mToast.setGravity(builder.gravity, builder.offsetX, builder.offsetY);
@@ -142,33 +137,39 @@ public class SuperToast {
     }
 
     private void setViewContent(View view, Builder builder) {
-        GradientDrawable drawable = new GradientDrawable();
-        drawable.setColor(builder.backgroundColor);
-        drawable.setStroke(DimensUtils.dip2px(getApp(), 1f), getBackgroundShen(builder.backgroundColor));
-        drawable.setCornerRadius(DimensUtils.dip2px(getApp(), 4));
-        ViewCompat.setBackground(view, drawable);
+        if (!builder.isCustom) {
+            if (builder.backGroundDrawable != null) {
+                ViewCompat.setBackground(view, builder.backGroundDrawable);
+            } else {
+                GradientDrawable drawable = new GradientDrawable();
+                drawable.setColor(builder.backgroundColor);
+                drawable.setStroke(DimensUtils.dip2px(getApp(), 1f), getBackgroundShen(builder.backgroundColor));
+                drawable.setCornerRadius(DimensUtils.dip2px(getApp(), 4));
+            }
+            ImageView imageView = (ImageView) view.findViewById(R.id.img);
+            if (!builder.isShowIcon) {
+                imageView.setVisibility(View.GONE);
+            } else {
+                Drawable draw = ContextCompat.getDrawable(view.getContext(), builder.iconRes);
+                //自动设置文字颜色
+                if ("auto_tintColor".equals(imageView.getTag())) {
+                    draw = DrawableCompat.wrap(draw);
+                    DrawableCompat.setTint(draw, StatusBarCompat.isColorDrak(builder.backgroundColor) ? 0xffffffff :
+                            0xff000000);
+                }
+                imageView.setImageDrawable(draw);
 
-        ImageView imageView = (ImageView) view.findViewById(R.id.img);
-        if (!builder.isShowIcon) {
-            imageView.setVisibility(View.GONE);
-        } else {
-            Drawable draw = ContextCompat.getDrawable(view.getContext(), builder.iconRes);
+            }
+            TextView textView = (TextView) view.findViewById(R.id.msg);
             //自动设置文字颜色
-            if ("auto_tintColor".equals(imageView.getTag())) {
-                draw = DrawableCompat.wrap(draw);
-                DrawableCompat.setTint(draw, StatusBarCompat.isColorDrak(builder.backgroundColor) ? 0xffffffff :
+            if ("auto_textColor".equals(textView.getTag())) {
+                textView.setTextColor(StatusBarCompat.isColorDrak(builder.backgroundColor) ? 0xffffffff :
                         0xff000000);
             }
-            imageView.setImageDrawable(draw);
-
+            textView.setText(builder.msg);
+        } else {
+            ((TextView) view.findViewById(R.id.msg)).setText(builder.msg);
         }
-        TextView textView = (TextView) view.findViewById(R.id.msg);
-        //自动设置文字颜色
-        if ("auto_textColor".equals(textView.getTag())) {
-            textView.setTextColor(StatusBarCompat.isColorDrak(builder.backgroundColor) ? 0xffffffff :
-                    0xff000000);
-        }
-        textView.setText(builder.msg);
     }
 
 
@@ -188,6 +189,11 @@ public class SuperToast {
         @DrawableRes
         private int iconRes = NO_RES;
         private int backgroundColor = NO_RES;
+        /**
+         * 背景
+         * 如果设置那么内部就不会设置其他背景
+         */
+        private Drawable backGroundDrawable = null;
         private String msg;
         private int duration = Toast.LENGTH_SHORT;
         private int gravity = CHANG_GRAVITY;
@@ -197,6 +203,10 @@ public class SuperToast {
         private int type = Info;
         @LayoutRes
         private int layoutId = R.layout.toast_super;
+        /**
+         * 这个Layout 是否是自定义的，如果是，那么工具就不会这是其他属性
+         */
+        private boolean isCustom = false;
         boolean isFinish = false;
         Activity activity;//要finish的activity
         Callback callback;//toast销毁的回调
@@ -221,6 +231,11 @@ public class SuperToast {
             return this;
         }
 
+        public Builder setBackgroundDrawable(Drawable backGroundDrawable) {
+            this.backGroundDrawable = backGroundDrawable;
+            return this;
+        }
+
 
         public Builder setDuration(int duration) {
             this.duration = duration;
@@ -234,6 +249,12 @@ public class SuperToast {
 
         public Builder setLayoutId(int layoutId) {
             this.layoutId = layoutId;
+            return this;
+        }
+
+        public Builder setLayoutId(int layoutId, boolean isCustom) {
+            this.layoutId = layoutId;
+            this.isCustom = isCustom;
             return this;
         }
 
