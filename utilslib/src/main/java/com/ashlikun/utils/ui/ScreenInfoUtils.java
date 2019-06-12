074df796c6c19f28019a8c5283d253e6a5c6cb4c
likun <496546144@qq.com>
2019-06-12 09:26:28 +0800

    扩展一些方法

--- utilslib/src/main/java/com/ashlikun/utils/ui/ScreenInfoUtils.java
+++ utilslib/src/main/java/com/ashlikun/utils/ui/ScreenInfoUtils.java
@@ -5,6 +5,7 @@ import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
 import com.ashlikun.utils.AppUtils;
+import com.ashlikun.utils.other.DimensUtils;
 
 /**
  * @author　　: 李坤
@@ -36,6 +37,85 @@ public class ScreenInfoUtils {
         return metric.heightPixels;
     }
 
+    public static int width() {
+        return widthSub(0);
+    }
+
+    /**
+     * @param subDp 减去的dp大小
+     * @return
+     */
+    public static int widthSubDp(int subDp) {
+        return widthSub(DimensUtils.dip2px(subDp));
+    }
+
+    /**
+     * @param subPx 减去的px大小
+     * @return
+     */
+    public static int widthSub(int subPx) {
+        if (AppUtils.getApp() == null) {
+            return 0;
+        }
+        DisplayMetrics displayMetrics = AppUtils.getApp().getResources().getDisplayMetrics();
+        return displayMetrics.widthPixels - subPx;
+    }
+
+    public static int height() {
+        return heightSub(0);
+    }
+
+    /**
+     * @param subDp 减去的dp大小
+     * @return
+     */
+    public static int heightSubDp(int subDp) {
+        return heightSub(DimensUtils.dip2px(subDp));
+    }
+
+    /**
+     * @param subPx 减去的px大小
+     * @return
+     */
+    public static int heightSub(int subPx) {
+        if (AppUtils.getApp() == null) {
+            return 0;
+        }
+        DisplayMetrics displayMetrics = AppUtils.getApp().getResources().getDisplayMetrics();
+        return displayMetrics.heightPixels - subPx;
+    }
+
+    /**
+     * 作者　　: 李坤
+     * 创建时间: 2017/7/5 13:29
+     * 邮箱　　：496546144@qq.com
+     * <p>
+     * 方法功能：获取状态栏高度
+     */
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    /**
+     * 作者　　: 李坤
+     * 创建时间: 2017/7/5 13:29
+     * 邮箱　　：496546144@qq.com
+     * <p>
+     * 方法功能：获取导航栏高度
+     */
+    public static int getNavigationBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
 
     public static float getDensity() {
         init();
