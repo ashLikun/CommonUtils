c3c01c0d5e01932ea09ed1279460bd930bdd5d09
likun <496546144@qq.com>
2019-09-23 14:27:54 +0800

    动画工具整理

--- utilslib/src/main/java/com/ashlikun/utils/animator/AnimUtils.java
+++ utilslib/src/main/java/com/ashlikun/utils/animator/AnimUtils.java
@@ -5,10 +5,11 @@ import android.animation.Keyframe;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
-import androidx.annotation.ColorInt;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.ColorInt;
 
 import com.ashlikun.utils.other.DimensUtils;
 
@@ -22,20 +23,23 @@ import com.ashlikun.utils.other.DimensUtils;
 
 public class AnimUtils {
     /**
-     * 作者　　: 李坤
-     * 创建时间: 2017/6/27 17:25
-     * <p>
      * 方法功能：上下抖动动画，用于提醒用户去点击
      *
      * @param scaleMax:缩放的最大值 0.85
      * @param rotation:转动角度   6
      */
-
-    public static void shakeUp(View view, float scaleMax, float rotation) {
-        getShakeUp(view, scaleMax, rotation).start();
-
+    public static ObjectAnimator shakeUp(View view, float scaleMax, float rotation) {
+        ObjectAnimator objectAnimator = getShakeUp(view, scaleMax, rotation);
+        objectAnimator.start();
+        return objectAnimator;
     }
 
+    /**
+     * 方法功能：上下抖动动画，用于提醒用户去点击
+     *
+     * @param scaleMax:缩放的最大值 0.85
+     * @param rotation:转动角度   6
+     */
     public static ObjectAnimator getShakeUp(View view, float scaleMax, float rotation) {
         // Keyframe是一个时间/值对，用于定义在某个时刻动画的状态
         // 在不同时间段的X轴0.8-1.1的缩放
@@ -81,16 +85,26 @@ public class AnimUtils {
     }
 
     /**
-     * 作者　　: 李坤
-     * 创建时间: 2017/6/27 17:26
-     * <p>
-     * 方法功能：左右抖动动画，用于表单验证失败
+     * 左右抖动动画，用于表单验证失败
      *
      * @param scaleMax:缩放最大    0.85
      * @param translation:平移距离 6
      */
 
-    public static void shakeLeft(View view, float scaleMax, float translation) {
+    public static ObjectAnimator shakeLeft(View view, float scaleMax, float translation) {
+        ObjectAnimator objectAnimator = getShakeLeft(view, scaleMax, translation);
+        objectAnimator.start();
+        return objectAnimator;
+    }
+
+    /**
+     * 左右抖动动画，用于表单验证失败
+     *
+     * @param scaleMax:缩放最大    0.85
+     * @param translation:平移距离 6
+     */
+
+    public static ObjectAnimator getShakeLeft(View view, float scaleMax, float translation) {
         translation = DimensUtils.dip2px(view.getContext(), translation);
         // Keyframe是一个时间/值对，用于定义在某个时刻动画的状态
         // 在不同时间段的X轴0.8-1.1的缩放
@@ -132,72 +146,78 @@ public class AnimUtils {
                 Keyframe.ofFloat(0.9f, translation), Keyframe.ofFloat(1f, 0f));
         ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
                 view, pvhScaleX, pvhScaleY, pvhRotation).setDuration(1000);
-        objectAnimator.start();
-
+        return objectAnimator;
     }
 
     /**
-     * 作者　　: 李坤
-     * 创建时间: 2017/6/27 17:27
-     * <p>
-     * 方法功能： 缩放动画，先放大，后缩小，   用于提醒用户状态的变化
+     * 缩放动画，先放大，后缩小，   用于提醒用户状态的变化
+     */
+    public static ObjectAnimator scaleAnim(View view) {
+        return scaleAnim(view, 1.3f, 0.5f, 800);
+    }
+
+    /**
+     * 缩放动画，先放大，后缩小，   用于提醒用户状态的变化
      *
      * @param scaleMax 缩放最大值
      * @param scaleMin 缩放最小值
      * @param duration 时间
      */
 
-    public static void scaleAnim(View view, float scaleMax, float scaleMin, int duration) {
+    public static ObjectAnimator scaleAnim(View view, float scaleMax, float scaleMin, int duration) {
+        ObjectAnimator objectAnimator = getScaleAnim(view, scaleMax, scaleMin, duration);
+        objectAnimator.start();
+        return objectAnimator;
+    }
 
+    /**
+     * 缩放动画，先放大，后缩小，   用于提醒用户状态的变化
+     *
+     * @param scaleMax 缩放最大值
+     * @param scaleMin 缩放最小值
+     * @param duration 时间
+     */
+
+    public static ObjectAnimator getScaleAnim(View view, float scaleMax, float scaleMin, int duration) {
         PropertyValuesHolder scx = PropertyValuesHolder.ofFloat("scaleX", 1, scaleMax, 1, scaleMin, 1);
         PropertyValuesHolder scY = PropertyValuesHolder.ofFloat("scaleY", 1, scaleMax, 1, scaleMin, 1);
-
         ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
                 view, scx, scY).setDuration(duration);
+        return objectAnimator;
+    }
+
+    public static ValueAnimator rotationAnim(View view) {
+        return rotationAnim(view, 180, 360, 800);
+    }
+
+    /**
+     * Y轴翻转
+     */
+
+    public static ObjectAnimator rotationAnim(View view, float start, float end, int duration) {
+        ObjectAnimator objectAnimator = getRotationAnim(view, start, end, duration);
         objectAnimator.start();
-
-
+        return objectAnimator;
     }
 
     /**
-     * 作者　　: 李坤
-     * 创建时间: 2017/6/27 17:27
-     * <p>
-     * 方法功能： 缩放动画，先放大，后缩小，   用于提醒用户状态的变化
-     */
-    public static void scaleAnim(View view) {
-        scaleAnim(view, 1.3f, 0.5f, 800);
-    }
-
-
-    /**
-     * 作者　　: 李坤
-     * 创建时间: 2016/9/2 16:33
-     * <p>
-     * 方法功能：Y轴翻转
+     * Y轴翻转
      */
 
-    public static void rotationAnim(View view, float start, float end, int duration) {
+    public static ObjectAnimator getRotationAnim(View view, float start, float end, int duration) {
 
         PropertyValuesHolder scx = PropertyValuesHolder.ofFloat("rotationY", start, end);
         ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
                 view, scx).setDuration(duration);
-        objectAnimator.start();
-
-    }
-
-    public static void rotationAnim(View view) {
-        rotationAnim(view, 180, 360, 800);
+        return objectAnimator;
     }
 
 
     /**
-     * 作者　　: 李坤
-     * 创建时间: 2016/10/12 10:01
-     * <p>
-     * 方法功能：改变textVIew 的字体大小动画
+     * 改变textVIew 的字体大小动画
+     * 不会start
      */
-    public static void updateTextSize(final TextView textView, float fromSize, float toSize) {
+    public static ValueAnimator updateTextSize(final TextView textView, float fromSize, float toSize) {
         ValueAnimator animator = ValueAnimator.ofFloat(fromSize, toSize);
         animator.setDuration(500);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -207,17 +227,15 @@ public class AnimUtils {
                 textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, animatedValue);
             }
         });
-        animator.start();
+        return animator;
     }
 
     /**
-     * 作者　　: 李坤
-     * 创建时间: 2016/10/12 10:02
-     * <p>
-     * 方法功能：更新view的透明度
+     * 更新view的透明度
+     * 不会start
      */
 
-    public static void updateAlpha(final View view, float fromValue, float toValue) {
+    public static ValueAnimator updateAlpha(final View view, float fromValue, float toValue) {
         ValueAnimator animator = ValueAnimator.ofFloat(fromValue, toValue);
         animator.setDuration(500);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -227,18 +245,16 @@ public class AnimUtils {
                 view.setAlpha(animatedValue);
             }
         });
-        animator.start();
+        return animator;
     }
 
     /**
-     * 作者　　: 李坤
-     * 创建时间: 2016/10/12 10:02
-     * <p>
-     * 方法功能：更新textView的字体颜色
+     * 更新textView的字体颜色
+     * 不会start
      */
 
-    public static void updateTextColor(final TextView textView, @ColorInt int fromColor,
-                                       @ColorInt int toColor) {
+    public static ValueAnimator updateTextColor(final TextView textView, @ColorInt int fromColor,
+                                                @ColorInt int toColor) {
         ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), fromColor, toColor);
         colorAnimation.setDuration(500);
         colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -247,18 +263,16 @@ public class AnimUtils {
                 textView.setTextColor((Integer) animator.getAnimatedValue());
             }
         });
-        colorAnimation.start();
+        return colorAnimation;
     }
 
     /**
-     * 作者　　: 李坤
-     * 创建时间: 2016/10/12 10:03
-     * <p>
-     * 方法功能：更新背景色
+     * 更新背景色
+     * 不会start
      */
 
-    public static void updateViewBackgroundColor(final View view, @ColorInt int fromColor,
-                                                 @ColorInt int toColor) {
+    public static ValueAnimator updateViewBackgroundColor(final View view, @ColorInt int fromColor,
+                                                          @ColorInt int toColor) {
         ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), fromColor, toColor);
         colorAnimation.setDuration(500);
         colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -267,8 +281,6 @@ public class AnimUtils {
                 view.setBackgroundColor((Integer) animator.getAnimatedValue());
             }
         });
-        colorAnimation.start();
+        return colorAnimation;
     }
-
-
 }
