d9d976eb0280c7320bb307763bdc4135f434ab3e
爱生活、李坤 <496546144@qq.com>
2021-01-05 14:44:08 +0800

    优化新增屏幕属性的扩展

--- utilslib/src/main/java/com/ashlikun/utils/ui/extend/UiExtend.kt
+++ utilslib/src/main/java/com/ashlikun/utils/ui/extend/UiExtend.kt
@@ -6,6 +6,7 @@ import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import com.ashlikun.utils.other.DimensUtils
 import com.ashlikun.utils.ui.ResUtils
+import com.ashlikun.utils.ui.ScreenInfoUtils
 import com.ashlikun.utils.ui.SuperToast
 import com.ashlikun.utils.ui.ToastUtils
 
@@ -18,24 +19,74 @@ import com.ashlikun.utils.ui.ToastUtils
  */
 inline val Int.dp
     get() = DimensUtils.dip2px(this.toFloat())
-
 inline val Float.dp
     get() = DimensUtils.dip2px(this)
+inline val Double.dp
+    get() = DimensUtils.dip2px(this.toFloat())
 
 inline val Int.sp
     get() = DimensUtils.sp2px(this.toFloat())
 inline val Float.sp
     get() = DimensUtils.sp2px(this)
+inline val Double.sp
+    get() = DimensUtils.sp2px(this.toFloat())
 
 inline val Int.px2dip
     get() = DimensUtils.px2dip(this.toFloat())
 inline val Float.px2dip
     get() = DimensUtils.px2dip(this)
+inline val Double.px2dip
+    get() = DimensUtils.px2dip(this.toFloat())
 
 inline val Int.px2sp
     get() = DimensUtils.px2sp(this.toFloat())
 inline val Float.px2sp
     get() = DimensUtils.px2sp(this)
+inline val Double.px2sp
+    get() = DimensUtils.px2sp(this.toFloat())
+
+/**
+ * [ScreenInfoUtils]
+ * 屏幕宽度的几倍
+ * 0.5.sw : 屏幕宽度的一半
+ * 1.sw：屏幕宽度
+ */
+inline val Double.sw
+    get() = (ScreenInfoUtils.width() * this).toInt()
+inline val Int.sw
+    get() = ScreenInfoUtils.width() * this
+
+/**
+ * [ScreenInfoUtils]
+ * 屏幕高度的几倍
+ * 0.5.sh : 屏幕高度的一半
+ * 1.sh：屏幕高度
+ */
+inline val Double.sh
+    get() = (ScreenInfoUtils.height() * this).toInt()
+inline val Int.sh
+    get() = ScreenInfoUtils.height() * this
+
+/**
+ * [ScreenInfoUtils]
+ * 屏幕宽度减去 dp
+ * 10.5.shSubDp : 屏幕宽度 减去 10.5dp
+ */
+inline val Double.whSubDp
+    get() = ScreenInfoUtils.widthSub(this.toInt())
+inline val Int.whSubDp
+    get() = ScreenInfoUtils.widthSub(this)
+
+/**
+ * [ScreenInfoUtils]
+ * 屏幕高度减去 dp
+ * 10.5.shSubDp : 屏幕高度 减去 10.5dp
+ */
+inline val Double.shSubDp
+    get() = ScreenInfoUtils.heightSubDp(this.toInt())
+inline val Int.shSubDp
+    get() = ScreenInfoUtils.heightSubDp(this)
+
 
 /**
  * ResUtils
