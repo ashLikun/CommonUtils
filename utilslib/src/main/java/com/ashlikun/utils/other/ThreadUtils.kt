72e3bcd6fff032b087a0f21fb2df62dab131e89c
爱生活、李坤 <496546144@qq.com>
2022-03-13 20:11:42 +0800

    优化

--- utilslib/src/main/java/com/ashlikun/utils/other/ThreadUtils.kt
+++ utilslib/src/main/java/com/ashlikun/utils/other/ThreadUtils.kt
@@ -1,6 +1,9 @@
 package com.ashlikun.utils.other
 
 import android.os.Looper
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.TimeUnit
 
 /**
  * @author　　: 李坤
@@ -10,7 +13,7 @@ import android.os.Looper
  * 功能介绍：线程的一些工具
  */
 
-object ThreadUtils {
+object ThreadUtils : ExecutorService {
     /**
      * 是否是主线程
      */
@@ -20,9 +23,47 @@ object ThreadUtils {
     /**
      * 在线程池中执行线程
      */
-    fun execute(command: Runnable) {
+    override fun execute(command: Runnable) {
         ThreadPoolManage.get().execute(command)
     }
+
+    override fun shutdown() {
+        ThreadPoolManage.get().shutdown()
+    }
+
+    override fun shutdownNow() = ThreadPoolManage.get().shutdownNow()
+
+    override fun isShutdown() = ThreadPoolManage.get().isShutdown()
+
+    override fun isTerminated() = ThreadPoolManage.get().isTerminated()
+
+    override fun awaitTermination(timeout: Long, unit: TimeUnit) =
+        ThreadPoolManage.get().awaitTermination(timeout, unit)
+
+    override fun <T : Any?> submit(task: Callable<T>) = ThreadPoolManage.get().submit(task)
+
+    override fun <T : Any?> submit(task: Runnable, result: T) =
+        ThreadPoolManage.get().submit(task, result)
+
+    override fun submit(task: Runnable) = ThreadPoolManage.get().submit(task)
+
+    override fun <T : Any?> invokeAll(tasks: MutableCollection<out Callable<T>>) =
+        ThreadPoolManage.get().invokeAll(tasks)
+
+    override fun <T : Any?> invokeAll(
+        tasks: MutableCollection<out Callable<T>>,
+        timeout: Long,
+        unit: TimeUnit
+    ) = ThreadPoolManage.get().invokeAll(tasks, timeout, unit)
+
+    override fun <T : Any?> invokeAny(tasks: MutableCollection<out Callable<T>>) =
+        ThreadPoolManage.get().invokeAny(tasks)
+
+    override fun <T : Any?> invokeAny(
+        tasks: MutableCollection<out Callable<T>>?,
+        timeout: Long,
+        unit: TimeUnit
+    ) = ThreadPoolManage.get().invokeAny(tasks, timeout, unit)
 
     /**
      * 切换到主线程
