a4b6dda7a5de7f0f004fca3051b4bdaefa51accd
爱生活、李坤 <496546144@qq.com>
2022-04-19 11:32:49 +0800

    优化

--- utilslib/src/main/java/com/ashlikun/utils/other/spannable/XClickableSpan.kt
+++ utilslib/src/main/java/com/ashlikun/utils/other/spannable/XClickableSpan.kt
@@ -14,15 +14,15 @@ import android.text.style.ClickableSpan
  */
 abstract class XClickableSpan(
     //文字颜色
-    open var color: Int = 0,
+    open var color: Int? = null,
+    //下划线
     open var underlineText: Boolean = false,
-    open var colorIsSet: Boolean = true
 ) : ClickableSpan() {
 
     override fun updateDrawState(ds: TextPaint) {
-        if (colorIsSet) {
+        if (color != null) {
             //设定的是span超链接的文本颜色，而不是点击后的颜色，点击后的背景颜色(HighLightColor)属于TextView的属性，Android4.0以上默认是淡绿色，低版本的是黄色。
-            ds.color = color
+            ds.color = color!!
         }
         ds.isUnderlineText = underlineText
         ds.clearShadowLayer()
